9b56209010cfc670bf0ba431776f34dac37569f3
mamontovavv <69858117+mamontovavv@users.noreply.github.com>
2020-08-18 23:42:43 +0400

    Add files via upload
    initial version

---
+++ Purchase.java
@@ -0,0 +1,11 @@
+public class Purchase  {
+    String hall;
+    int row;
+    int place;
+
+    public Purchase (String hall, int row, int place) {
+        this.hall = hall;
+        this.row = row;
+        this.place = place;
+    }
+}
